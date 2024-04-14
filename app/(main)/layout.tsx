0bbc94b9ad47a1247e0ea0159044cda46b4d51ff
Safal Pokharel <pokharelsafal66@gmail.com>
2024-04-14 18:34:20 +1000

    another commit

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,40 +8,40 @@ import { ToggleTheme } from '@/components/shared/ToggleTheme'
 
 const Layout =
   ({
-    children
-  }: Readonly<{ children: React.ReactNode }>) => {
-    const { userId } = auth()
+     children
+   }: Readonly<{ children: React.ReactNode }>) => {
+    const {userId} = auth ()
     return (
-      <div className={'min-h-screen bg-background'}>
-        <div className={'h-[80px] fixed w-full inset-y-0 z-50'}>
+      <div className={ 'min-h-screen bg-background' }>
+        <div className={ 'h-[80px] fixed w-full inset-y-0 z-50' }>
           <div
-            className={'flex items-center justify-between h-full p-4 border-b shadow-sm lg:px-16'}>
-            <MobileSidebar />
-            <div className={'hidden md:flex items-center space-x-4'}>
-              <Link href={'/'} className={'text-gray-500'}>
-                <Image src="/logo.svg" alt="logo" width={100} height={100} />
+            className={ 'flex items-center justify-between h-full p-4 border-b shadow-sm lg:px-16' }>
+            <MobileSidebar/>
+            <div className={ 'hidden md:flex items-center space-x-4' }>
+              <Link href={ '/' } className={ 'text-gray-500' }>
+                <Image src="/logo.svg" alt="logo" width={ 100 } height={ 100 }/>
               </Link>
             </div>
-            <div className={'flex items-center space-x-2'}>
-              <ToggleTheme />
-              {userId ?
-                <div className={'flex items-center justify-between space-x-4'}>
-                  <Button variant={'outline'}>
-                    <Link href={'/dashboard'} className={'text-theme text-sm'}>
+            <div className={ 'flex items-center space-x-2' }>
+              <ToggleTheme/>
+              { userId ?
+                <div className={ 'flex items-center justify-between space-x-4' }>
+                  <Link href={ '/dashboard' }>
+                    <Button variant={ 'outline' }>
                       Dashboard
-                    </Link>
-                  </Button>
-                  <UserButton afterSignOutUrl={'/'} />
+                    </Button>
+                  </Link>
+                  <UserButton afterSignOutUrl={ '/' }/>
                 </div>
                 :
-                <div className={'flex items-center justify-between space-x-4'}>
-                  <Link href={'/sign-in'}>
-                    <Button variant={'outline'} size={'sm'}>
+                <div className={ 'flex items-center justify-between space-x-4' }>
+                  <Link href={ '/sign-in' }>
+                    <Button variant={ 'outline' } size={ 'sm' }>
                       Login
                     </Button>
                   </Link>
-                  <Link href={'/sign-up'}>
-                    <Button variant={'outline'} size={'sm'} >
+                  <Link href={ '/sign-up' }>
+                    <Button variant={ 'outline' } size={ 'sm' }>
                       Sign up
                     </Button>
                   </Link>
@@ -50,8 +50,8 @@ const Layout =
             </div>
           </div>
         </div>
-        <div className={'mt-[80px]'}>
-          {children}
+        <div className={ 'mt-[80px]' }>
+          { children }
         </div>
       </div>
     )
